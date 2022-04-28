ab4865b0cdb865704c28199ca888a7d093a1d9ad
Cyber_Kuryber <urosstanic25@gmail.com>
2022-04-28 19:34:44 +0200

    implemented examination schedule window without dialogs

--- HealthInstitution/GUI/PatientWindows/PatientScheduleWindow.xaml.cs
+++ HealthInstitution/GUI/PatientWindows/PatientScheduleWindow.xaml.cs
@@ -11,6 +11,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using HealthInstitution.Core.SystemUsers.Patients.Model;
+using HealthInstitution.Core.Examinations.Model;
+using HealthInstitution.Core.Examinations.Repository;
 
 namespace HealthInstitution.GUI.PatientWindows
 {
@@ -19,13 +22,43 @@ namespace HealthInstitution.GUI.PatientWindows
     /// </summary>
     public partial class PatientScheduleWindow : Window
     {
-        public PatientScheduleWindow()
+        public PatientScheduleWindow(Patient loggedPatient)
         {
             InitializeComponent();
         }
 
+        public void LoadGridRows(Patient loggedPatient)
+        {
+            foreach (Examination examination in ExaminationRepository.GetInstance().examinations)
+            {
+                if (examination.medicalRecord.patient.username == loggedPatient.username)
+
+                    dataGrid.Items.Add(examination);
+            }
+        }
+
         private void addButton_click(object sender, RoutedEventArgs e)
         {
+            AddExaminationDialog addExaminationDialog = new AddExaminationDialog();
+            addExaminationDialog.ShowDialog();
+        }
+
+        private void editButton_click(object sender, RoutedEventArgs e)
+        {
+            Examination selectedExamination = (Examination)dataGrid.SelectedItem;
+            EditExaminationDialog editExaminationDialog = new EditExaminationDialog(selectedExamination);
+            editExaminationDialog.ShowDialog();
+        }
+
+        private void deleteButton_click(object sender, RoutedEventArgs e)
+        {
+            if (System.Windows.MessageBox.Show("Are you sure you want to delete selected examination", "Question", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+            {
+                Examination selectedExamination = (Examination)dataGrid.SelectedItem;
+                dataGrid.Items.Remove(selectedExamination);
+                examinationRepository.DeleteExamination(selectedExamination.id);
+                //dodaj isto za doktora.
+            }
         }
     }
 }
