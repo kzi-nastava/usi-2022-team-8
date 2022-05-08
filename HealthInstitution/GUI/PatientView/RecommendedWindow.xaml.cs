f6216fd1e7376fdb3757975d699b3fa4bb06680c
Cyber_Kuryber <urosstanic25@gmail.com>
2022-05-08 18:24:43 +0200

    created gui window

---
+++ HealthInstitution/GUI/PatientView/RecommendedWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HealthInstitution.GUI.PatientView
+{
+    /// <summary>
+    /// Interaction logic for RecommendedWindow.xaml
+    /// </summary>
+    public partial class RecommendedWindow : Window
+    {
+        public RecommendedWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
