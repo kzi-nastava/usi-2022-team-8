95dbc62a2e00a1d52c7c050f4facf0f4ab622da0
Jovan Najdovski <j.najdovski@gmail.com>
2022-06-03 23:57:55 +0200

    IRepository created

--- HealthInstitution/Core/IRepository.cs
+++ HealthInstitution/Core/IRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,17 @@ using System.Threading.Tasks;
 
 namespace HealthInstitution.Core
 {
-    internal class IRepository
+    public interface IRepository<T> where T : class
     {
+        public List<T> GetAll();
+
+        public void Save();
+
+        public void LoadFromFile();
+
+        public void Parse(JToken? examination);
+
+
+
     }
 }
