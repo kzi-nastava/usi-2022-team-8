ee6b1f7881c8051db2c29d86d0138b5a136319ca
Cyber_Kuryber <urosstanic25@gmail.com>
2022-06-13 01:20:35 +0200

    +slightly better patient gui packaging

---
+++ HealthInstitution/GUI/PatientView/PatientPolllDialog.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HealthInstitution.GUI.PatientView
+{
+    /// <summary>
+    /// Interaction logic for PatientPolllDialog.xaml
+    /// </summary>
+    public partial class PatientPolllDialog : Window
+    {
+        public PatientPolllDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
