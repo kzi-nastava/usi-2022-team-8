a0d062e5b7e3ff8567690c19c1ee75cd4e363fe0
Nikola Savic <savic.sv21.2020@uns.ac.rs>
2022-06-12 16:14:50 +0200

    Added rest request repository

--- HealthInstitution/Core/RestRequests/Model/RestRequest.cs
+++ HealthInstitution/Core/RestRequests/Model/RestRequest.cs
@@ -4,13 +4,24 @@ namespace HealthInstitution.Core.RestRequests.Model;
 
 public class RestRequest
 {
+    public int Id { get; set; }
     public Doctor Doctor { get; set; }
     public String Reason { get; set; }
     public DateOnly StartDate { get; set; }
     public int DaysDuration { get; set; }
-    public RestRequestState state { get; set; }
+    public RestRequestState State { get; set; }
     public bool IsUrgent { get; set; }
-    
+
+    public RestRequest(int id, Doctor doctor, string reason, DateOnly startDate, int daysDuration, RestRequestState state, bool isUrgent)
+    {
+        Id = id;
+        Doctor = doctor;
+        Reason = reason;
+        StartDate = startDate;
+        DaysDuration = daysDuration;
+        State = state;
+        IsUrgent = isUrgent;
+    }
 }
 
 public enum RestRequestState
---
+++ HealthInstitution/Core/RestRequests/Repository/RestRequestRepository.cs
@@ -0,0 +1,161 @@
+﻿using HealthInstitution.Core.RestRequests.Model;
+using HealthInstitution.Core.SystemUsers.Doctors.Model;
+using HealthInstitution.Core.SystemUsers.Doctors.Repository;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace HealthInstitution.Core.RestRequests.Repository
+{
+    public class RestRequestRepository
+    {
+        private String _fileName;
+        public int _maxId { get; set; }
+        public List<RestRequest> RestRequests { get; set; }
+        public Dictionary<int, RestRequest> RestRequestsById { get; set; }
+
+        private JsonSerializerOptions _options = new JsonSerializerOptions
+        {
+            Converters = { new JsonStringEnumConverter() },
+            PropertyNameCaseInsensitive = true
+        };
+        private RestRequestRepository(String fileName)
+        {
+            this._fileName = fileName;
+            this.RestRequests = new List<RestRequest>();
+            this.RestRequestsById = new Dictionary<int, RestRequest>();
+            this._maxId = 0;
+            this.LoadFromFile();
+        }
+        private static RestRequestRepository s_instance = null;
+        public static RestRequestRepository GetInstance()
+        {
+            {
+                if (s_instance == null)
+                {
+                    s_instance = new RestRequestRepository(@"..\..\..\Data\JSON\RestRequests.json");
+                }
+                return s_instance;
+            }
+        }
+
+        private RestRequest Parse(JToken? RestRequest)
+        {
+            int id = (int)RestRequest["id"];
+            string username = (string)RestRequest["doctor"];
+            Doctor doctor = DoctorRepository.GetInstance().GetById(username);
+            string reason = (string)RestRequest["reason"];
+            DateTime startDate = (DateTime)RestRequest["startDate"];
+            int daysDuration = (int)RestRequest["daysDuration"];
+            RestRequestState state;
+            Enum.TryParse(RestRequest["state"].ToString(), out state);
+            bool urgent = (bool)RestRequest["urgent"];
+            
+            return new RestRequest(id,doctor,reason,DateOnly.FromDateTime(startDate), daysDuration,state,urgent);
+        }
+        public void LoadFromFile()
+        {
+            var allRestRequests = JArray.Parse(File.ReadAllText(this._fileName));
+            foreach (var RestRequest in allRestRequests)
+            {
+                RestRequest loadedRestRequest = Parse(RestRequest);
+                int id = loadedRestRequest.Id;
+                if (id > _maxId) { _maxId = id; }
+
+                this.RestRequests.Add(loadedRestRequest);
+                this.RestRequestsById.Add(id, loadedRestRequest);
+            }
+        }
+
+        private List<dynamic> PrepareForSerialization()
+        {
+            List<dynamic> reducedRestRequests = new List<dynamic>();
+            foreach (RestRequest RestRequest in this.RestRequests)
+            {
+                reducedRestRequests.Add(new
+                {
+                    id = RestRequest.Id,
+                    doctor = RestRequest.Doctor.Username,
+                    reason = RestRequest.Reason,
+                    startDate = RestRequest.StartDate,
+                    daysDuration = RestRequest.DaysDuration,
+                    state = RestRequest.State,
+                    urgent=RestRequest.IsUrgent
+                });
+            }
+            return reducedRestRequests;
+        }
+        public void Save()
+        {
+            List<dynamic> reducedRestRequests = PrepareForSerialization();
+            var allRestRequests = JsonSerializer.Serialize(reducedRestRequests, _options);
+            File.WriteAllText(this._fileName, allRestRequests);
+        }
+
+        public void AcceptScheduleEditRequests(RestRequest restRequest)
+        {
+            restRequest.State = RestRequestState.Accepted;
+            Save();
+        }
+
+        public void RejectScheduleEditRequests(RestRequest restRequest)
+        {
+            restRequest.State = RestRequestState.Rejected;
+            Save();
+        }
+
+        public List<RestRequest> GetAll()
+        {
+            return this.RestRequests;
+        }
+
+        public RestRequest GetById(int id)
+        {
+            if (RestRequestsById.ContainsKey(id))
+            {
+                return RestRequestsById[id];
+            }
+            return null;
+        }
+
+        private void AddToCollections(RestRequest RestRequest)
+        {
+            RestRequest.Doctor.RestRequests.Add(RestRequest);
+            RestRequests.Add(RestRequest);
+            RestRequestsById.Add(RestRequest.Id, RestRequest);
+        }
+
+        public void Add(RestRequest RestRequest)
+        {
+            int id = ++this._maxId;
+            RestRequest.Id = id;
+            AddToCollections(RestRequest);
+            Save();
+            DoctorRepository.GetInstance().Save();
+        }
+
+        public void Update(int id, RestRequest byRestRequest)
+        {
+            RestRequest RestRequest = GetById(id);
+            RestRequest.Reason = byRestRequest.Reason;
+            RestRequest.StartDate = byRestRequest.StartDate;
+            RestRequest.DaysDuration = byRestRequest.DaysDuration;
+            this.RestRequestsById[id] = RestRequest;
+            Save();
+        }
+        public void Delete(int id)
+        {
+            RestRequest RestRequest = RestRequestsById[id];
+            this.RestRequests.Remove(RestRequest);
+            this.RestRequestsById.Remove(id);
+            Save();
+            DoctorRepository.GetInstance().Save();
+        }
+    }
+}
